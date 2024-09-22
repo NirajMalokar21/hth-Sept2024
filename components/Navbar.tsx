540ff45e37b06ad0d690d74e2b2ac0cc2c7530b4
Niraj Malokar <nirajmalokar@gmail.com>
2024-09-22 18:44:02 -0400

    Add ThemeProvider using useContext hook

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='border-2 border-red-500 bg-red-700 w-full p-2 px-4 h-[70px] flex flex-row items-center justify-between'>
+        <h1>RAVEN MAP</h1>
+        <button className='bg-white p-1 rounded-sm px-2 '>Log In</button>
     </div>
   )
 }
