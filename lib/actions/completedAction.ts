e179ec78e6acbb76acd6810a9b131c36cdfc437c
John Lu <john.lu10212004@gmail.com>
2024-09-23 12:09:11 -0400

    Used Prisma's implicit join and filter commands to simplify code

--- lib/actions/completedAction.ts
+++ lib/actions/completedAction.ts
@@ -6,33 +6,29 @@ export async function getAllCompleted() {
 
 //Get all users by completed nodes
 export async function getUsersByNodeCompleted(name: string) {
-    //Alternatively, find id of node.
-    const node = await prisma.node.findMany({
-        where: { name },
-    });
-    const node_id = node[0].id;
-    //Then get list of user_ids based on the id of the theme in the associative "completed" table
-    const user_ids = await prisma.completed.findMany({
-        where: { node_id },
-    });
-    //Then get all users based on list of user_ids
     return await prisma.user.findMany({
-        where: { id: { in: user_ids.map((completed) => completed.user_id) } },
+        where: {
+            completed: {
+                some: {
+                    Node: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
 
 export async function getNodesByUser(name: string) {
-    //Find id of user.
-    const user = await prisma.user.findMany({
-        where: { name },
-    });
-    const user_id = user[0].id;
-    //Then get list of node_ids based on the id of the user in the associative "completed" table
-    const node_ids = await prisma.completed.findMany({
-        where: { user_id },
-    });
-    //Then get all nodes based on list of node_ids
     return await prisma.node.findMany({
-        where: { id: { in: node_ids.map((completed) => completed.node_id) } },
+        where: {
+            completedBy: {
+                some: {
+                    User: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
--- lib/actions/requiresAction.ts
+++ lib/actions/requiresAction.ts
@@ -6,34 +6,30 @@ export async function getAllRequirements() {
 
 //Get all required nodes for a specific node
 export async function getAllPreReqNodesbyNode(name: string) {
-    //first, find id of node
-    const node = await prisma.node.findMany({
-        where: { name },
-    });
-    const node_id = node[0].id;
-    //Then, find list of pre_req_node_ids in the "requires" table
-    const pre_req_node_ids = await prisma.requires.findMany({
-        where: { node_id },
-    });
-    //Then find all the nodes with ids from pre_req_node_ids list
     return await prisma.node.findMany({
-        where: { id: { in: pre_req_node_ids.map((requires) => requires.pre_req_node_id) } },
+        where: {
+            requiredBy: {
+                some: {
+                    Node: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
 
 //Get all nodes that require a specific node
 export async function getAllNodesRequiredByPreReqNode(name: string) {
-    //first, find id of node
-    const preReqNode = await prisma.node.findMany({
-        where: { name },
-    });
-    const pre_req_node_id = preReqNode[0].id;
-    //Then, find list of node_ids in the "requires" table where "node_id"=node
-    const node_ids = await prisma.requires.findMany({
-        where: { pre_req_node_id },
-    });
-    //Then find all nodes with ids from node_ids list
     return await prisma.node.findMany({
-        where: { id: { in: node_ids.map((requires) => requires.node_id) } },
+        where: {
+            requires: {
+                some: {
+                    Node: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
