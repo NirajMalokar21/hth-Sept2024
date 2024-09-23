392c8e4405f0531a81e4fb98464525506ca749a8
John Lu <john.lu10212004@gmail.com>
2024-09-23 12:09:11 -0400

    Used Prisma's implicit join and filter commands to simplify code

--- lib/actions/followsAction.ts
+++ lib/actions/followsAction.ts
@@ -6,34 +6,30 @@ export async function getAllFollows() {
 
 //Get all users who follow a theme
 export async function getUsersByTheme(name: string) {
-    //Find id of theme.
-    const theme = await prisma.theme.findMany({
-        where: { name },
-    });
-
-    const theme_id = theme[0].id;
-
-    //Then get list of user_ids based on the id of the theme in the associative "follows" table
-    const follows = await prisma.follows.findMany({
-        where: { theme_id },
-    });
-    //Then get all users based on list of user_ids
+    //Join the tables and filter(some) by theme's name
     return await prisma.user.findMany({
-        where: { id: { in: follows.map((follow) => follow.user_id) } },
+        where: {
+            follows: {
+                some: {
+                    Theme: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
 
-export async function getThemesFollowedByUser(name: string) {
-    //Find id of user.
-    const user = await prisma.user.findMany({
-        where: { name },
-    });
-    //Then get list of theme_ids based on the id of the user in the associative "follows" table
-    const follows = await prisma.follows.findMany({
-        where: { user_id: user[0].id },
-    });
-    //Then get all themes based on list of theme_ids
+export async function getThemesFollowedByUser(userName: string) {
     return await prisma.theme.findMany({
-        where: { id: { in: follows.map((follow) => follow.theme_id) } },
+        where: {
+            followedBy: {
+                some: {
+                    User: {
+                        name: userName,
+                    },
+                },
+            },
+        },
     });
 }
--- lib/actions/hasAction.ts
+++ lib/actions/hasAction.ts
@@ -5,33 +5,29 @@ export async function getAllHas() {
 }
 
 export async function getThemesByNode(name: string) {
-    //Find id of node.
-    const node = await prisma.node.findMany({
-        where: { name },
-    });
-    const node_id = node[0].id;
-    //Then get list of theme_ids based on the id of the node in the associative "has" table
-    const theme_ids = await prisma.has.findMany({
-        where: { node_id },
-    });
-    //Then get all themes based on list of theme_ids
     return await prisma.theme.findMany({
-        where: { id: { in: theme_ids.map((has) => has.theme_id) } },
+        where: {
+            has: {
+                some: {
+                    Node: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
 
 export async function getNodesByTheme(name: string) {
-    //Find id of theme.
-    const theme = await prisma.theme.findMany({
-        where: { name },
-    });
-    const theme_id = theme[0].id;
-    //Then get list of node_ids based on the id of the theme in the associative "has" table
-    const node_ids = await prisma.has.findMany({
-        where: { theme_id },
-    });
-    //Then get all nodes based on list of node_ids
     return await prisma.node.findMany({
-        where: { id: { in: node_ids.map((has) => has.node_id) } },
+        where: {
+            hadBy: {
+                some: {
+                    Theme: {
+                        name,
+                    },
+                },
+            },
+        },
     });
 }
