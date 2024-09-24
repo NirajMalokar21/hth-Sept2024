2e3a38d3149467f0c5fc8a4df03899aa0521aa63
NirajMalokar21 <nirajmalokar@gmail.com>
2024-09-24 11:43:25 -0400

    Implement auth using clerkJS

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Theme from './Theme'
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className='border-2 border-red-500 bg-red-700 w-full p-2 px-4 h-[70px] flex flex-row items-center justify-between'>
         <h1 className='h1-bold text-white'>RAVEN MAP</h1>
-        <div className='flex flex-row items-center justify-center gap-4'>
+        <div className='flex flex-row items-center justify-center gap-2'>
             <Theme />
-            <button className='text-white text-xl font-semibold p-1 rounded-sm px-2 '>Log-In</button>
+            <SignedOut>
+              <Link href="/sign-in" className='flex flex-row gap-3 bg-red-700 text-white'>
+                  <span className='text-xl font-semibold'>Log In /</span>
+              </Link>
+              <Link href="/sign-up" className='flex flex-row bg-red-700 text-white'>
+                  <span className='text-xl font-semibold'>Sign up</span>
+              </Link>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         </div>
     </div>
   )
