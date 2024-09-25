3c59332778a65b56538469fcfaa33480bb4913c2
John Lu <john.lu10212004@gmail.com>
2024-09-25 00:16:40 -0400

    added 2 exclamation marks

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
     return (
-      <div className="">
-        <h1>Hello, World!</h1>
-      </div>
+        <div className="">
+            <h1>Hello, World!!!</h1>
+        </div>
     );
-  }
-  
+}
