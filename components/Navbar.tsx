ef8a1537b69fa5162424e99a4e5ef59ea7406da9
JohnLu2004 <JohnLu10212004@gmail.com>
2024-09-25 00:05:40 -0400

    Removed unnecessary imports and fixed string-replacing-boolean error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import Theme from './Theme'
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import Theme from "./Theme";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <div className='border-2 border-red-500 bg-red-700 w-full p-2 px-4 h-[70px] flex flex-row items-center justify-between'>
-        <h1 className='h1-bold text-white'>RAVEN MAP</h1>
-        <div className='flex flex-row items-center justify-center gap-2'>
-            <Theme />
-            <SignedOut>
-              <Link href="/sign-in" className='flex flex-row gap-3 bg-red-700 text-white'>
-                  <span className='text-xl font-semibold'>Log In /</span>
-              </Link>
-              <Link href="/sign-up" className='flex flex-row bg-red-700 text-white'>
-                  <span className='text-xl font-semibold'>Sign up</span>
-              </Link>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+    return (
+        <div className="border-2 border-red-500 bg-red-700 w-full p-2 px-4 h-[70px] flex flex-row items-center justify-between">
+            <h1 className="h1-bold text-white">RAVEN MAP</h1>
+            <div className="flex flex-row items-center justify-center gap-2">
+                <Theme />
+                <SignedOut>
+                    <Link href="/sign-in" className="flex flex-row gap-3 bg-red-700 text-white">
+                        <span className="text-xl font-semibold">Log In /</span>
+                    </Link>
+                    <Link href="/sign-up" className="flex flex-row bg-red-700 text-white">
+                        <span className="text-xl font-semibold">Sign up</span>
+                    </Link>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,84 +1,80 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
-  theme: {
-  	container: {
-  		center: 'true',
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	extend: {
-  		colors: {
-  			background: {
-  				'400': '#CFCFC9',
-  				'500': '#D8D8D4',
-  				'700': '#E2E2DF',
-  				'800': '#ECECEA',
-  				'850': '#F5F5F4',
-  				'900': '#FFFFFF'
-  			},
-  			primary: {
-  				'100': '#87C08F',
-  				'500': '#54A05F',
-  				'600': '#4D9358'
-  			},
-  			dark: {
-  				'100': '#000000',
-  				'200': '#0F1117',
-  				'300': '#151821',
-  				'400': '#212734',
-  				'500': '#101012'
-  			},
-  			light: {
-  				'400': '#858EAD',
-  				'500': '#7B8EC8',
-  				'700': '#DCE3F1',
-  				'800': '#F4F6F8',
-  				'850': '#FDFDFD',
-  				'900': '#FFFFFF'
-  			},
-  			'accent-blue': '#1DA1F2'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+    darkMode: ["class"],
+    content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            colors: {
+                background: {
+                    "400": "#CFCFC9",
+                    "500": "#D8D8D4",
+                    "700": "#E2E2DF",
+                    "800": "#ECECEA",
+                    "850": "#F5F5F4",
+                    "900": "#FFFFFF",
+                },
+                primary: {
+                    "100": "#87C08F",
+                    "500": "#54A05F",
+                    "600": "#4D9358",
+                },
+                dark: {
+                    "100": "#000000",
+                    "200": "#0F1117",
+                    "300": "#151821",
+                    "400": "#212734",
+                    "500": "#101012",
+                },
+                light: {
+                    "400": "#858EAD",
+                    "500": "#7B8EC8",
+                    "700": "#DCE3F1",
+                    "800": "#F4F6F8",
+                    "850": "#FDFDFD",
+                    "900": "#FFFFFF",
+                },
+                "accent-blue": "#1DA1F2",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: {
+                        height: "0",
+                    },
+                    to: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                },
+                "accordion-up": {
+                    from: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                    to: {
+                        height: "0",
+                    },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+        },
+    },
+    plugins: [tailwindcssAnimate],
+} satisfies Config;
 
-export default config
+export default config;
